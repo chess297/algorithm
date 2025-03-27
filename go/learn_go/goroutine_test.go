b62c66b8fa9a9db9d93adb3b16988d0ba21d5558
chess <viky.chen@outlook.com>
2025-03-28 04:05:47 +0800

    fix(go): goroutine is unordered

--- go/learn_go/goroutine_test.go
+++ go/learn_go/goroutine_test.go
@@ -13,22 +13,22 @@ import (
 
 var (
 	students = map[int64]*student{
-		1: &student{
+		1: {
 			id:   1,
 			name: "s1",
 			age:  13,
 		},
-		2: &student{
+		2: {
 			id:   2,
 			name: "s2",
 			age:  13,
 		},
-		3: &student{
+		3: {
 			id:   3,
 			name: "s3",
 			age:  13,
 		},
-		4: &student{
+		4: {
 			id:   4,
 			name: "s4",
 			age:  13,
@@ -54,30 +54,31 @@ func TestMain(t *testing.T) {
 		ids := []int64{1, 2}
 		result := map[int64]int64{}
 		slice := []int64{}
-		for _, id := range ids {
+		// for _, id := range ids { // for range 是无序的，所以id 可能会有问题
+		for i := 0; i <= len(ids)-1; i++ {
 			wg.Add(1)
+			id := ids[i]
 			go func(id int64) {
 				defer func() {
-					wg.Done()
 					mutex.Unlock()
+					wg.Done()
 				}()
-				s := getStudent(id)
 				mutex.Lock()
+				s := getStudent(id)
 				result[id] = s.age
-				slice = append(slice, s.id)
-				t.Logf(" s.id %d s.name %s", s.id, s.name)
+				slice = append(slice, id)
 			}(id)
 		}
 		wg.Wait()
 		if result[1] != 13 {
 			t.Errorf("result[1] is %d want 15", result[1])
 		}
-		if slice[0] != 1 {
-			t.Errorf("slice[0] is %d want 1", slice[0])
-		}
-		if slice[1] != 2 {
-			t.Errorf("slice[1] is %d want 2", slice[1])
-		}
+		// if slice[0] != 1 {
+		// 	t.Errorf("slice[0] is %d want 1", slice[0])
+		// }
+		// if slice[1] != 2 {
+		// 	t.Errorf("slice[1] is %d want 2", slice[1])
+		// }
 	})
 
 }
--- go/main.go
+++ go/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "go-algorithm/data_structure"
+
 func main() {
 	println("Hello, Golang Algorithm ~")
+	a := data_structure.NewArray([]int{}, 10)
+	a.DeleteLast()
 }
